0695d3e855d6d662bf001851ce49e42ebe55d9c6
Deekshith918 <deekshithmunuganti@gmail.com>
2025-07-12 12:59:29 +0530

    Update Question1.py

--- Machine Learning/Question1.py
+++ Machine Learning/Question1.py
@@ -1,7 +1,10 @@
+#taking a list 
 list1= [2, 7, 4, 1, 3, 6]
+#creating a empty list ,later we append the pair whose sum is equal to 10 
 pairs = []
 for i in range(len(list1)):
     for j in range(i + 1, len(list1)):
+#check the corresponding two numbers sum is equal to 10  we add the pair to pairs list
      if list1[i] + list1[j] == 10:
             pairs.append((list1[i], list1[j]))
 print("Pairs with sum 10:", pairs)
